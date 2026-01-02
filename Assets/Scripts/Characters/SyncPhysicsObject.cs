e854032a6f7114eba585796fca33356153908c5b
RileyWren <zainab.42847@gmail.com>
2026-01-02 18:14:38 +0600

    Animating the character

---
+++ Assets/Scripts/Characters/SyncPhysicsObject.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncPhysicsObject : MonoBehaviour
+{
+    Rigidbody rigidbody3D;
+    ConfigurableJoint joint;
+
+    [SerializeField]
+    Rigidbody animatedRigidbody3D;
+
+    [SerializeField]
+    bool syncAnimation = false;
+
+    Quaternion startLocalRotation;
+
+    void Awake()
+    {
+        rigidbody3D = GetComponent<Rigidbody>();
+        joint = GetComponent<ConfigurableJoint>();
+        startLocalRotation = joint.transform.localRotation;
+    }
+
+    public void UpdateJointFromAnimation()
+    {
+        if (!syncAnimation)
+        return;
+        ConfigurableJointExtensions.SetTargetRotationLocal(joint, animatedRigidbody3D.transform.localRotation, startLocalRotation);
+    }
+}
